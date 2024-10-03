5877e3f368785c644608d63607e8f647a2e79f56
Peter Munandi <nashpeter33@gmail.com>
2024-10-03 16:17:42 +0300

    app theme setup

--- lib/utilities/theme/app_theme.dart
+++ lib/utilities/theme/app_theme.dart
@@ -4,4 +4,42 @@ import 'package:splendlens_fe/utilities/utilities.dart';
 class AppTheme implements BaseTheme {
   @override
   ThemeData get appTheme => ThemeData();
+
+  @override
+  TextStyle get blackBoldNormalStyle => const TextStyle(
+        color: Colors.black,
+        fontWeight: FontWeight.bold,
+      );
+
+  @override
+  TextStyle get blackBoldSubheadlineStyle => const TextStyle(
+      fontWeight: FontWeight.bold, fontSize: 16.0, color: Colors.black);
+
+  @override
+  TextStyle get subheadlineStyle =>
+      const TextStyle(fontSize: 16.0, color: Colors.white);
+
+  @override
+  TextStyle get blackNormalStyle => const TextStyle(fontSize: 12.0);
+
+  @override
+  TextStyle get boldHeadlineStyle => const TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: 24,
+      );
+
+  @override
+  TextStyle get whiteBoldHeadlineStyle => const TextStyle(
+      fontWeight: FontWeight.bold, fontSize: 24, color: Colors.white);
+
+  @override
+  TextStyle get formFieldTextStyle => const TextStyle();
+
+  @override
+  TextStyle get whiteSubheadlineStyle => const TextStyle(
+      fontSize: 18.0, color: Colors.white54, fontWeight: FontWeight.w900);
+
+  @override
+  TextStyle get whiteNormallineStyle =>
+      TextStyle(fontSize: 12, color: Colors.white);
 }
--- lib/utilities/theme/base_theme.dart
+++ lib/utilities/theme/base_theme.dart
@@ -2,4 +2,15 @@ import 'package:flutter/material.dart';
 
 abstract class BaseTheme {
   ThemeData get appTheme;
+
+  // Text
+  TextStyle get blackBoldNormalStyle;
+  TextStyle get blackBoldSubheadlineStyle;
+  TextStyle get subheadlineStyle;
+  TextStyle get boldHeadlineStyle;
+  TextStyle get formFieldTextStyle;
+  TextStyle get blackNormalStyle;
+  TextStyle get whiteBoldHeadlineStyle;
+  TextStyle get whiteSubheadlineStyle;
+  TextStyle get whiteNormallineStyle;
 }

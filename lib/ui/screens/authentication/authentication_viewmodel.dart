f542f87781e4b4f6e1210c723c7eadf5c64138c6
Peter Munandi <nashpeter33@gmail.com>
2024-10-11 06:51:02 +0300

    added anackbar

--- lib/ui/screens/authentication/authentication_viewmodel.dart
+++ lib/ui/screens/authentication/authentication_viewmodel.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:awesome_snackbar_content/awesome_snackbar_content.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -35,7 +36,20 @@ class AuthenticationViewModel extends ChangeNotifier {
   String errorMessage = '';
 
   void handleError(BuildContext context, String message) {
-    debugPrint(message);
+    final snackBar = SnackBar(
+      elevation: 0,
+      behavior: SnackBarBehavior.floating,
+      backgroundColor: Colors.transparent,
+      content: AwesomeSnackbarContent(
+        title: 'On Snap!',
+        message: message,
+        contentType: ContentType.failure,
+      ),
+    );
+
+    ScaffoldMessenger.of(context)
+      ..hideCurrentSnackBar()
+      ..showSnackBar(snackBar);
     _isLoading = false;
     notifyListeners();
   }
@@ -57,8 +71,20 @@ class AuthenticationViewModel extends ChangeNotifier {
       debugPrint(jsonEncode(loginBody));
       await saveUserCredentials(jsonEncode(loginBody));
     } else {
-      // to handle
-      debugPrint(response?.errorMessage);
+      final snackBar = SnackBar(
+        elevation: 0,
+        behavior: SnackBarBehavior.floating,
+        backgroundColor: Colors.transparent,
+        content: AwesomeSnackbarContent(
+          title: '',
+          message: '${response!.errorMessage}',
+          contentType: ContentType.warning,
+        ),
+      );
+
+      ScaffoldMessenger.of(context)
+        ..hideCurrentSnackBar()
+        ..showSnackBar(snackBar);
     }
   }
 

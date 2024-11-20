a3ec2002592659aa9594531cb1159b14ef9a32d2
Peter Munandi <nashpeter33@gmail.com>
2024-11-20 06:47:51 +0300

    updated expense endpoint

--- lib/core/data/remote/network/base_api_service.dart
+++ lib/core/data/remote/network/base_api_service.dart
@@ -1,7 +1,9 @@
+import 'package:splendlens_fe/core/models/models.dart';
+
 abstract class BaseApiService {
   final String baseUrl = 'http://127.0.0.1:8000';
 
   Future<dynamic> login(String url, Map<String, dynamic> body);
   Future<dynamic> getUser(String url, String key);
-  Future<dynamic> createExpense(String url, Map<String, dynamic> body);
+  Future<dynamic> createExpense(String url, Expense body, String key);
 }

94aa736347bc67d0a5142981f4920d6126ce91d6
Peter Munandi <nashpeter33@gmail.com>
2025-02-25 06:14:01 +0300

    login form validation

--- lib/view/screens/authentication/login/components/login_form.dart
+++ lib/view/screens/authentication/login/components/login_form.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:splendlens_fe/core/utilities/utilities.dart';
+import 'package:splendlens_fe/core/data/response/auth_response.dart';
 import 'package:splendlens_fe/core/models/requests/login_model.dart';
 import 'package:splendlens_fe/viewmodel/authentication_viewmodel.dart';
+import 'package:awesome_snackbar_content/awesome_snackbar_content.dart';
 
 class LoginForm extends StatefulWidget {
   const LoginForm({super.key});
@@ -16,10 +19,33 @@ class _LoginFormState extends State<LoginForm> {
   AuthenticationViewModel _authenticationViewModel = AuthenticationViewModel();
   bool obsecure = true;
 
-  handleLogin(context, String? username, String? password) async {
+  handleLogin(String? username, String? password) async {
+    if (!_formKey.currentState!.validate()) {
+      return;
+    }
     final loginModel = LoginModel(username: username, password: password);
 
-    await _authenticationViewModel.login(context, loginModel.toJson());
+    await _authenticationViewModel.login(loginModel.toJson());
+
+    if (_authenticationViewModel.status == AuthStatus.authenticated) {
+      if (mounted) context.go('/');
+    } else {
+      final snackBar = SnackBar(
+        elevation: 0,
+        behavior: SnackBarBehavior.floating,
+        backgroundColor: const Color.fromARGB(0, 91, 82, 82),
+        content: AwesomeSnackbarContent(
+          title: 'Oh Snap!',
+          message: _authenticationViewModel.errorMessage,
+          contentType: ContentType.warning,
+        ),
+      );
+      if (mounted) {
+        ScaffoldMessenger.of(context)
+          ..hideCurrentSnackBar()
+          ..showSnackBar(snackBar);
+      }
+    }
   }
 
   @override
@@ -49,6 +75,8 @@ class _LoginFormState extends State<LoginForm> {
               decoration: const InputDecoration(
                   hintText: 'Username', border: OutlineInputBorder()),
               controller: _usernameController,
+              validator: (value) => Validators.notEmpty(value, 'your username'),
+              autovalidateMode: AutovalidateMode.onUserInteraction,
             ),
             const SizedBox(
               height: 16.0,
@@ -65,6 +93,8 @@ class _LoginFormState extends State<LoginForm> {
                   )),
               controller: _passwordController,
               obscureText: obsecure,
+              validator: (value) => Validators.notEmpty(value, 'your password'),
+              autovalidateMode: AutovalidateMode.onUserInteraction,
             ),
             const SizedBox(
               height: 16.0,
@@ -82,7 +112,7 @@ class _LoginFormState extends State<LoginForm> {
                         'Sign In',
                         style: TextStyle(color: AppTheme().white),
                       ),
-                      onPressed: () => handleLogin(context,
+                      onPressed: () => handleLogin(
                           _usernameController?.text, _passwordController?.text),
                     ),
                   )

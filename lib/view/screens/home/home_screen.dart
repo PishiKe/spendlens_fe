2405a3c73c40a1c6507d34959a1f7cd805995e3d
Peter Munandi <nashpeter33@gmail.com>
2025-06-22 16:40:04 +0300

    moved from from bottom modal sheet

--- lib/view/screens/home/home_screen.dart
+++ lib/view/screens/home/home_screen.dart
@@ -1,11 +1,9 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 import 'package:splendlens_fe/view/view.dart';
 import 'package:splendlens_fe/viewmodel/viewmodel.dart';
 import 'package:splendlens_fe/core/utilities/utilities.dart';
-import 'dart:io' show Platform;
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -21,39 +19,7 @@ class _HomeScreenState extends State<HomeScreen> {
   final expenseNameController = TextEditingController();
   final expenseDateController = TextEditingController();
   DateTime? expenseDate = DateTime.now();
-  final _formKey = GlobalKey<FormState>();
 
-  void handleAddExpense() {
-    if (!_formKey.currentState!.validate()) {
-      return;
-    }
-    final Map<String, dynamic> formData = {
-      "amount": int.parse(amountController.text),
-      "date": expenseDate.toString(),
-      "description": descriptionController.text,
-      "name": expenseNameController.text,
-      "user": _homeViewModel.user?.id
-    };
-
-    _homeViewModel.addExpense(formData).then((value) {
-      if (mounted) context.pop();
-    });
-  }
-
-  Future<void> _selectExpenseDate() async{
-    final DateTime? picked = await showDatePicker(
-      context: context,
-      firstDate: DateTime.now().subtract(const Duration(days: 365)),
-      lastDate: DateTime.now().add( const Duration(days: 365)
-    ));
-
-    if (picked != null && picked != expenseDate){
-      setState(() {
-        expenseDate = picked;
-        expenseDateController.text = '${picked.toLocal()}';
-      });
-    }
-  }
 
   @override
   void initState() {
@@ -111,99 +77,10 @@ class _HomeScreenState extends State<HomeScreen> {
             Icons.add,
             color: AppTheme().white,
           ),
-          onPressed: () {
-            showModalBottomSheet(
-                context: context,
-                builder: (BuildContext context) {
-                  return SizedBox.expand(
-                    child: bottomSheet(),
-                  );
-                });
-          },
+          onPressed: () => context.go('/addExpense'),
         ),
         floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       );
     });
-  }
-
-  Padding bottomSheet() {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 24.0),
-      child: Column(
-        children: [
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              'Add new expense',
-              style: AppTheme().blackBoldNormalStyle.copyWith(fontSize: 20.0),
-            ),
-          ),
-          Form(
-              key: _formKey,
-              child: Column(
-                children: [
-                  TextFormField(
-                    keyboardType: TextInputType.number,
-                    controller: amountController,
-                    decoration: const InputDecoration(label: Text('Amount')),
-                    validator: (value) => Validators.notEmpty(value, 'amount'),
-                  ),
-                  const SizedBox(
-                    height: 16.0,
-                  ),
-                  TextFormField(
-                    controller: expenseNameController,
-                    decoration:
-                        const InputDecoration(label: Text('Expense name')),
-                    validator: (value) =>
-                        Validators.notEmpty(value, 'an expense name'),
-                  ),
-                  const SizedBox(
-                    height: 16.0,
-                  ),
-                  TextFormField(
-                    controller: descriptionController,
-                    decoration:
-                        const InputDecoration(label: Text('Description')),
-                  ),
-                  const SizedBox(
-                    height: 24.0,
-                  ),
-                  Platform.isIOS
-                      ? SizedBox(
-                          height: 100,
-                          child: CupertinoDatePicker(
-                            initialDateTime: expenseDate,
-                            onDateTimeChanged: (DateTime newDate) {
-                              setState(() {
-                                expenseDate = newDate;
-                              });
-                            },
-                          ),
-                        )
-                      : TextFormField(
-                          controller: expenseDateController,
-                          decoration: InputDecoration(
-                            label:const Text('Date'),
-                            suffixIcon: Icon(
-                              Icons.calendar_month,
-                              color: AppTheme().black,
-                            ),
-                          ),
-                        onTap: _selectExpenseDate,
-                      ),
-                  const SizedBox(
-                    height: 16.0,
-                  ),
-                  SizedBox(
-                      width: double.infinity,
-                      height: 50.0,
-                      child: CustomButton(
-                          onPressed: handleAddExpense, text: 'Add'))
-                ],
-              ))
-        ],
-      ),
-    );
   }
 }
